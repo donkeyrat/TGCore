54e24476459771e4a076ba706707b1f9c6624233
donkeyrat <darklordbananas@gmail.com>
2026-01-03 13:53:21 -0800

    updated version number

--- TGMain.cs
+++ TGMain.cs
@@ -14,7 +14,7 @@ using UnityEngine.SceneManagement;
 
 namespace TGCore 
 {
-	[BepInPlugin("teamgrad.core", "Team Grad Core", "1.0.7")]
+	[BepInPlugin("teamgrad.core", "Team Grad Core", "1.1.0")]
 	public class TGMain : BaseUnityPlugin
 	{
 		private void Awake()
